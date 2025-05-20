c7102811348218cfa87b685b1a7db07ca41d66f2
Gregoire Descamps <gregoire.descamps@lacatholille.fr>
2025-05-20 15:17:06 +0200

    Minor final Correction
    removed max_target_seqs argument from blastx command

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ fasta_to_ORF = ORFExtractor(fastaFileName)
 ORF_Dict = fasta_to_ORF.extract()
 
 # run BLASTx against putative ORF extracted
-fasta_to_ORF.blastx(database=blastdb, outputfile=blastoutput, taxids=9606, evalue=0.05, outfmt=7, max_target_seqs=1 )
+fasta_to_ORF.blastx(database=blastdb, outputfile=blastoutput, taxids=9606, evalue=0.05, outfmt=7)
 
 # Validate ORF found and annotate source sequences
 fasta_to_ORF.orf_validate()
